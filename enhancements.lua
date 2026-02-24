68f3c8ba3ca686f868dcf5d3940988397b47efc3
DoctorWafflePhD <111939112+DoctorWafflePhD@users.noreply.github.com>
2026-02-23 18:14:11 -0600

    Unimplemented Ripple, no good ideas for an enhancement RN

--- enhancements.lua
+++ enhancements.lua
@@ -5,6 +5,15 @@ SMODS.Atlas {
     py = 95,
 }
 
+-- Ripple (NYI)
+if false then
+    SMODS.Enhancement {
+    key = "ripple",
+    atlas = "wafflemod_enhancementAtlas",
+    pos = {x=1,y=0}
+}
+end
+
 -- Scribbled
 SMODS.Enhancement {
     key = "scribbled",
@@ -14,10 +23,3 @@ local card_is_face_ref = Card.is_face
 function Card:is_face(from_boss)
     return card_is_face_ref(self, from_boss) or SMODS.has_enhancement(self, "m_wafflemod_scribbled")
 end
-
--- Ripple
-SMODS.Enhancement {
-    key = "ripple",
-    atlas = "wafflemod_enhancementAtlas",
-    pos = {x=1,y=0}
-}
