c83680e875678c714e0d3fd6b6f0c2da1bfc9421
DoctorWafflePhD <111939112+DoctorWafflePhD@users.noreply.github.com>
2026-07-09 09:51:17 -0500

    - The Ox: $2 on trigger -> $4 on trigger

--- jokers.lua
+++ jokers.lua
@@ -2515,7 +2515,7 @@ WaffleMod.BossJoker {
     key = "ox",
     config = {
         extra = {
-            dollars = 2
+            dollars = 4
         }
     },
     loc_vars = function(self, info_queue, card)
